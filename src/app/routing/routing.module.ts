8bf991eb06888dc98ded14fdbad1235858340c0d
Agathangelidis Grigorios <gregory82gr@yahoo.gr>
2024-04-07 16:14:45 +0300

    initial commit

---
+++ src/app/routing/routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { NotFoundComponent } from '../error-pages/not-found/not-found.component';
+import { ServerErrorComponent } from '../error-pages/server-error/server-error.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'owner', loadChildren: () => import('./../owner/owner.module').then(m => m.OwnerModule) }, 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '404', component: NotFoundComponent}, 
+  { path: '**', redirectTo: '/404', pathMatch: 'full'},
+  { path: '500', component: ServerErrorComponent },
+
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class RoutingModule { }
